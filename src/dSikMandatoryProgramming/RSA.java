a3a73ed9136f8ed16fd8f552c50c632f01fcbefe
Bardur R. Guttesen <bardurrunason@gmail.com>
2016-04-22 00:48:00 +0200

    Initial commit

---
+++ src/dSikMandatoryProgramming/RSA.java
@@ -0,0 +1,19 @@
+package dSikMandatoryProgramming;
+
+public class RSA {
+	public static void main(String[] args) {
+		
+	}
+	
+	public String decrypt (String stringToDecrypt) {
+		return stringToDecrypt;
+	}
+	
+	public String encrypt (String stringToEncrypt) {
+		return stringToEncrypt;
+	}
+	
+	public String keyGen (String s) {
+		return s;
+	}
+}
