e4d7b9dd62af77e0c7e5813e388b8bc8ddd92bb7
Bardur R. Guttesen <bardurrunason@gmail.com>
2016-04-25 16:58:36 +0200

    Still not working... added a few changes and changed from Random to SecureRandom

--- src/dSikMandatoryProgramming/RSA.java
+++ src/dSikMandatoryProgramming/RSA.java
@@ -1,7 +1,7 @@
 package dSikMandatoryProgramming;
 
 import java.math.BigInteger;
-import java.util.Random;
+import java.security.SecureRandom;
 
 public class RSA {
 	private static final BigInteger e = new BigInteger("3");
@@ -9,60 +9,73 @@ public class RSA {
 	private BigInteger n = null;
 	
 	public static void main(String[] args) {
-		RSA rsa = new RSA();
-		rsa.keyGen(128);
-		System.out.println("n = "+rsa.getN());
+		RSA rsa = new RSA(100);
 		BigInteger message = new BigInteger("100");
-		System.out.println("Message: "+message);
-		BigInteger encryptedMessage = rsa.encrypt(message);
-		System.out.println("Encrypted message: "+encryptedMessage);
-		BigInteger decryptedMessage = rsa.decrypt(encryptedMessage);
-		System.out.println("Decrypted message: "+decryptedMessage);
+		System.out.println("message: "+message);
+		System.out.println("bitlength of message: "+message.bitLength());
+		// encrypt and decrypt the message
+		rsa.decrypt(rsa.encrypt(message));
 	}
 	
-	public BigInteger decrypt (BigInteger numToDecrypt) {
-		BigInteger decrypted = numToDecrypt.modPow(d, n);
+	public RSA(int k) {
+		this.keyGen(k);
+	}
+	
+	public BigInteger decrypt (BigInteger c) {
+		// c^d mod n
+		BigInteger decrypted = c.modPow(d, n);
+		System.out.println("decrypted msg: "+decrypted);
 		return decrypted;
 	}
 	
-	public BigInteger encrypt (BigInteger numToEncrypt) {
-		BigInteger encrypted = numToEncrypt.modPow(e, n);
-		return encrypted;
+	// 0 <= numToEncrypt <= n-1
+	public BigInteger encrypt (BigInteger m) {
+		// c = m^e mod n
+		BigInteger c = m.modPow(e, n);
+		System.out.println("encrypted msg: "+c);
+		return c;
 	}
 	
 	/**
-	 * @precondition: k >= 128
+	 * @precondition: k >= 2000
 	 */
-	public void keyGen (int k) {
-		Random random = new Random();
+	private void keyGen (int k) {
+		SecureRandom random = new SecureRandom();
 		
 		int max = k/2+k/10;
 		int min = k/2-k/10;
 		int bitLengthOfP = random.nextInt(max - min + 1) + min;
 		int bitLengthOfQ = k - bitLengthOfP;
+		System.out.println("bitlength of q: "+bitLengthOfQ);
+		System.out.println("bitlength of p: "+bitLengthOfP);
 		
 		BigInteger p = new BigInteger(bitLengthOfP, random);
 		BigInteger q = new BigInteger(bitLengthOfQ, random);
-		BigInteger one = new BigInteger("1");
 
+		// bitlength of n should equal k
 		while ((n = p.multiply(q)).bitLength() != k) {
+			// ensure that gcd(q-1, e) == 1
 			do {
 				q = new BigInteger(bitLengthOfQ, random);
-			} while (!(q.subtract(one)).gcd(e).equals(one));
-
+			} while (!(q.subtract(BigInteger.ONE)).gcd(e).equals(BigInteger.ONE));
+			
+			// ensure that gcd(p-1, e) == 1
 			do {
 				p = new BigInteger(bitLengthOfP, random);
-			} while (!(p.subtract(one)).gcd(e).equals(one));
+			} while (!(p.subtract(BigInteger.ONE)).gcd(e).equals(BigInteger.ONE));
+			
+			// n = p*q
 			n = p.multiply(q);
 		}
+		System.out.println("q: "+q);
+		System.out.println("p: "+p);
+		System.out.println("n: "+n);
 		
-		BigInteger phiOfN = p.subtract(one).multiply(q.subtract(one));
-		System.out.println("phiOfN: "+phiOfN);
+		// d = e^{-1} mod (p-1)(q-1)
+		BigInteger qMinusOne = q.subtract(BigInteger.ONE); System.out.println("qMinusOne: "+qMinusOne);
+		BigInteger pMinusOne = p.subtract(BigInteger.ONE); System.out.println("pMinusOne: "+pMinusOne);
+		BigInteger phiOfN = pMinusOne.multiply(qMinusOne);
 		d = e.modInverse(phiOfN);
-		System.out.println("Benny: "+d.multiply(e).mod(phiOfN));
-	}
-	
-	public BigInteger getN() {
-		return n;
+		System.out.println("d: "+d);
 	}
 }
