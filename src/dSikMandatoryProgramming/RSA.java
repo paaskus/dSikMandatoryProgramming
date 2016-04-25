71c6dd6db6fe99851fbcea0e766f4cb24e8f0d79
Bardur R. Guttesen <bardurrunason@gmail.com>
2016-04-25 17:34:11 +0200

    It's not beautiful, but it works!

--- src/dSikMandatoryProgramming/RSA.java
+++ src/dSikMandatoryProgramming/RSA.java
@@ -4,78 +4,79 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 
 public class RSA {
-	private static final BigInteger e = new BigInteger("3");
-	private BigInteger d;
-	private BigInteger n = null;
-	
+	private BigInteger e = new BigInteger("3");
+	private BigInteger d, n = null;
+	private BigInteger zero = BigInteger.ZERO;
+	private BigInteger one = BigInteger.ONE;
+
 	public static void main(String[] args) {
 		RSA rsa = new RSA(100);
 		BigInteger message = new BigInteger("100");
-		System.out.println("message: "+message);
-		System.out.println("bitlength of message: "+message.bitLength());
+		System.out.println("message: " + message);
+		System.out.println("bitlength of message: " + message.bitLength());
 		// encrypt and decrypt the message
 		rsa.decrypt(rsa.encrypt(message));
 	}
-	
+
 	public RSA(int k) {
 		this.keyGen(k);
 	}
-	
-	public BigInteger decrypt (BigInteger c) {
+
+	public BigInteger decrypt(BigInteger c) {
 		// c^d mod n
 		BigInteger decrypted = c.modPow(d, n);
-		System.out.println("decrypted msg: "+decrypted);
+		System.out.println("decrypted msg: " + decrypted);
 		return decrypted;
 	}
-	
+
 	// 0 <= numToEncrypt <= n-1
-	public BigInteger encrypt (BigInteger m) {
+	public BigInteger encrypt(BigInteger m) {
 		// c = m^e mod n
 		BigInteger c = m.modPow(e, n);
-		System.out.println("encrypted msg: "+c);
+		System.out.println("encrypted msg: " + c);
 		return c;
 	}
-	
+
 	/**
 	 * @precondition: k >= 2000
 	 */
-	private void keyGen (int k) {
+	private void keyGen(int k) {
 		SecureRandom random = new SecureRandom();
-		
-		int max = k/2+k/10;
-		int min = k/2-k/10;
-		int bitLengthOfP = random.nextInt(max - min + 1) + min;
-		int bitLengthOfQ = k - bitLengthOfP;
-		System.out.println("bitlength of q: "+bitLengthOfQ);
-		System.out.println("bitlength of p: "+bitLengthOfP);
-		
-		BigInteger p = new BigInteger(bitLengthOfP, random);
-		BigInteger q = new BigInteger(bitLengthOfQ, random);
+
+		int bitLengthOfQ = k / 2;
+		int bitLengthOfP = k - bitLengthOfQ;
+
+		BigInteger q = new BigInteger(bitLengthOfQ, 100, random);
+		BigInteger p = new BigInteger(bitLengthOfP, 100, random);
 
 		// bitlength of n should equal k
-		while ((n = p.multiply(q)).bitLength() != k) {
+		while ((n = p.multiply(q)).bitLength() != k  || q.subtract(one).multiply(p.subtract(one)).mod(e).equals(zero)) {
 			// ensure that gcd(q-1, e) == 1
 			do {
-				q = new BigInteger(bitLengthOfQ, random);
-			} while (!(q.subtract(BigInteger.ONE)).gcd(e).equals(BigInteger.ONE));
-			
+				q = new BigInteger(bitLengthOfQ, 100, random);
+			} while (!(q.subtract(one)).gcd(e).equals(one));
+
 			// ensure that gcd(p-1, e) == 1
 			do {
-				p = new BigInteger(bitLengthOfP, random);
-			} while (!(p.subtract(BigInteger.ONE)).gcd(e).equals(BigInteger.ONE));
-			
+				p = new BigInteger(bitLengthOfP, 100, random);
+			} while (!(p.subtract(one)).gcd(e).equals(one));
+
 			// n = p*q
 			n = p.multiply(q);
 		}
-		System.out.println("q: "+q);
-		System.out.println("p: "+p);
-		System.out.println("n: "+n);
 		
+		System.out.println("bitlength of n: " + n.bitLength());
+
+		System.out.println("q: " + q);
+		System.out.println("p: " + p);
+		System.out.println("n: " + n);
+
 		// d = e^{-1} mod (p-1)(q-1)
-		BigInteger qMinusOne = q.subtract(BigInteger.ONE); System.out.println("qMinusOne: "+qMinusOne);
-		BigInteger pMinusOne = p.subtract(BigInteger.ONE); System.out.println("pMinusOne: "+pMinusOne);
-		BigInteger phiOfN = pMinusOne.multiply(qMinusOne);
-		d = e.modInverse(phiOfN);
-		System.out.println("d: "+d);
+		BigInteger qMinusOne = q.subtract(one);
+		System.out.println("qMinusOne: " + qMinusOne);
+		BigInteger pMinusOne = p.subtract(one);
+		System.out.println("pMinusOne: " + pMinusOne);
+		System.out.println("pq mod e: "+qMinusOne.multiply(pMinusOne).mod(e));
+		d = e.modInverse(qMinusOne.multiply(pMinusOne));
 	}
 }
