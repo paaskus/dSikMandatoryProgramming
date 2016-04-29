745c16fa7c5b0429a8332e43f802f83ab2a1bc50
Bardur R. Guttesen <bardurrunason@gmail.com>
2016-04-29 04:11:24 +0200

    week1 done

--- src/dSikMandatoryProgramming/RSA.java
+++ src/dSikMandatoryProgramming/RSA.java
@@ -1,8 +1,6 @@
 package dSikMandatoryProgramming;
 
 import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
 public class RSA {
@@ -11,7 +9,6 @@ public class RSA {
 	private static final BigInteger one = BigInteger.ONE;
 	private int k = 2000;
 	private BigInteger d, n = null;
-	private MessageDigest md = null;
 
 	public RSA(int bitLength) {
 		this.k = bitLength;
@@ -33,39 +30,6 @@ public class RSA {
 	 */
 	public BigInteger encrypt(BigInteger m) {
 		return m.modPow(e, n);
-	}
-
-	/*
-	 * Sign m with the secret key (which means using the decrypt method).
-	 */
-	public BigInteger sign(BigInteger m) {
-		return decrypt(sha256(m));
-	}
-
-	/*
-	 * Verify that m is from an authentic source by verifying c with the public key
-	 * (which means using the encrypt method).
-	 */
-	public boolean verify(BigInteger m, BigInteger c) {
-		return sha256(m).equals(encrypt(c));
-	}
-	
-	/*
-	 * Calculates the SHA-256 hash of the number given as argument.
-	 */
-	private BigInteger sha256(BigInteger numToHash) {
-		// Instantiate a SHA-256 MessageDigest if it is not done already
-		if (md == null) {
-			try {
-				md = MessageDigest.getInstance("SHA-256");
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
-		}
-		// 
-		byte[] hashed = md.digest(numToHash.toByteArray());
-		// md.reset();
-		return new BigInteger(1, hashed);
 	}
 
 	/*
@@ -117,10 +81,9 @@ public class RSA {
 	 */
 	public static void main(String[] args) {
 		// Only for testing; reads messages as input from program arguments
-		boolean error = false;
 		RSA rsa = new RSA(2000);
 		for (int i = 0; i < args.length; i++) {
-			if (i == 0) System.out.println("\n\n\n=== POSTIVE TEST ENCRYPT/DECRYPT AND SIGNATURES ===\n");
+			if (i == 0) System.out.println("\n\n\n=== POSTIVE TEST ENCRYPT/DECRYPT ===\n");
 			System.out.println("================================");
 			BigInteger message = new BigInteger(args[i]);
 			BigInteger encryptedMessage = rsa.encrypt(message);
@@ -128,31 +91,8 @@ public class RSA {
 			System.out.println("message: " + message);
 			System.out.println("encrypted message: " + encryptedMessage);
 			System.out.println("decrypted message: " + decryptedMessage);
-			BigInteger signedMessage = rsa.sign(message);
-			boolean verified = rsa.verify(message, signedMessage);
-			if (!verified) error = false;
-			System.out.println("verified: "+verified+" <== should be 'true'");
 			System.out.println("================================");
 			if (i != args.length - 1) System.out.println("\n");
 		}
-		
-		for (int i = 0; i < args.length; i = i + 2) {
-			if (i == 0) System.out.println("\n\n\n=== NEGATIVE TEST OF SIGNATURES ===\n");
-			if (i+1 >= args.length) break;
-			System.out.println("================================");
-			BigInteger message = new BigInteger(args[i]);
-			System.out.println("message: " + message);
-			BigInteger signedMessage = rsa.sign(message);
-			BigInteger fakeSignature = rsa.sign(new BigInteger(args[i+1]));
-			System.out.println("signed message: "+signedMessage);
-			System.out.println("fake signature: "+fakeSignature);
-			boolean verified = rsa.verify(message, fakeSignature);
-			if (verified) error = false;
-			System.out.println("verified: "+verified+" <== should be 'false'");
-			System.out.println("================================");
-			if (i != args.length - 1) System.out.println("\n");
-		}
-		if (error) System.out.println("A mistake was found; the program does not work as it should.");
-		else System.out.println("No mistakes were found; the encryption/decryption and signatures worked as expected.");
 	}
 }
